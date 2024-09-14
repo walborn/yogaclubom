9efec6ef6754f28eb14195cf217641be21f9dab3
Boris <yuzhakov.brois@gmail.com>
2024-09-15 00:05:50 +0300

    feat: add schedule with broken but old-like styles

---
+++ app/schedule/lessons.ts
@@ -0,0 +1,278 @@
+export type Lesson = {
+  id: string;
+  user: {
+    id: string;
+    name: string;
+    picture: string;
+  },
+  weekday: number;
+  time: number;
+  duration: number;
+  category: string;
+  title: string;
+  master: string;
+  alternate: string;
+  disabled: boolean;
+  hidden: boolean;
+  level: string;
+  room: number;
+  note?: string;
+  from: string;
+  to: string;
+}
+
+export const weekdays = [ 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс' ]
+export const format = (minutes: number) => (mm => `${`0${(minutes - mm) / 60}`.slice(-2)}:${`0${mm}`.slice(-2)}`)(minutes % 60)
+// Получить номер текущего дня недели: 0 - Пн, ..., 6 - Вс
+export const getDay = () => {
+  const now = new Date()
+  const offset = (now.getTimezoneOffset() + 180) * 60 * 1000
+  return (new Date(now.getTime() + offset).getDay() + 6) % 7
+}
+export const lessons = [
+  {
+    day: 'Пн',
+    time: '10:00',
+    duration: 120,
+    title: 'Аштанга-йога (майсор-класс)',
+    level: 'Сложный',
+    master: 'Ирина',
+  },
+  {
+    day: 'Пн',
+    time: '12:15',
+    duration: 90,
+    title: 'Хатха-йога для начинающих',
+    level: 'Лёгкий',
+    master: 'Наталья',
+    note: 'по предварительной записи',
+  },
+  {
+    day: 'Пн',
+    time: '16:15',
+    duration: 90,
+    title: 'Джит Кун-До детская группа',
+    level: '5-14 лет',
+    master: 'Сергей',
+  },
+  {
+    day: 'Пн',
+    time: '18:00',
+    duration: 120,
+    title: 'Аштанга-йога (майсор-класс)',
+    level: 'Сложный',
+    master: 'Ирина',
+  },
+  {
+    day: 'Пн',
+    time: '20:15',
+    duration: 90,
+    title: 'Хатха-йога для начинающих',
+    level: 'Лёгкий',
+    master: 'Наталья',
+  },
+  {
+    day: 'Вт',
+    time: '09:30',
+    duration: 90,
+    title: 'Хатха-йога для начинающих',
+    level: 'Лёгкий',
+    master: 'Надежда',
+    note: 'по предварительной записи',
+  },
+  {
+    day: 'Вт',
+    time: '11:00',
+    duration: 90,
+    title: 'Йога для беременных',
+    level: 'Лёгкий',
+    master: 'Надежда',
+    note: 'по предварительной записи',
+  },
+  {
+    day: 'Вт',
+    time: '18:00',
+    duration: 90,
+    title: 'Хатха-йога для начинающих',
+    level: 'Лёгкий',
+    master: 'Анжелика',
+    note: 'по предварительной записи',
+  },
+  {
+    day: 'Вт',
+    time: '19:30',
+    duration: 90,
+    title: 'Хатха-йога',
+    level: 'Средний',
+    master: 'Нина',
+  },
+  {
+    day: 'Ср',
+    time: '18:00',
+    duration: 120,
+    title: 'Аштанга-йога (майсор-класс)',
+    level: 'Сложный',
+    master: 'Ирина',
+  },
+  {
+    day: 'Ср',
+    time: '20:15',
+    duration: 90,
+    title: 'Хатха-йога',
+    level: 'средний',
+    master: 'Евгения',
+  },
+  {
+    day: 'Чт',
+    time: '10:00',
+    duration: 120,
+    title: 'Аштанга-йога (майсор-класс)',
+    level: 'Сложный',
+    master: 'Ирина',
+  },
+  {
+    day: 'Чт',
+    time: '12:15',
+    duration: 90,
+    title: 'Хатха-йога для начинающих',
+    level: 'Лёгкий',
+    master: 'Наталья',
+    note: 'по предварительной записи',
+  },
+  {
+    day: 'Чт',
+    time: '18:00',
+    duration: 90,
+    title: 'Йога-терапия',
+    level: 'Лёгкий',
+    master: 'Анжелика',
+    note: 'по предварительной записи',
+  },
+  {
+    day: 'Чт',
+    time: '19:30',
+    duration: 90,
+    title: 'Хатха-йога',
+    level: 'Средний',
+    master: 'Нина',
+  },
+  {
+    day: 'Пт',
+    time: '09:30',
+    duration: 90,
+    title: 'Хатха-йога для начинающих',
+    level: 'Лёгкий',
+    master: 'Надежда',
+    note: 'по предварительной записи',
+  },
+  {
+    day: 'Пт',
+    time: '11:00',
+    duration: 90,
+    title: 'Йога для беременных',
+    level: 'Лёгкий',
+    master: 'Надежда',
+    note: 'по предварительной записи',
+  },
+  {
+    day: 'Пт',
+    time: '16:15',
+    duration: 90,
+    title: 'Джит Кун-До детская группа',
+    level: '5-14 лет',
+    master: 'Сергей',
+  },
+  {
+    day: 'Пт',
+    time: '18:00',
+    duration: 120,
+    title: 'Аштанга-йога для начинающих',
+    level: 'Средний',
+    master: 'Ирина',
+  },
+  {
+    day: 'Пт',
+    time: '20:15',
+    duration: 90,
+    title: 'Хатха-йога для начинающих',
+    level: 'Лёгкий',
+    master: 'Наталья',
+  },
+  {
+    day: 'Сб',
+    time: '09:00',
+    duration: 90,
+    title: 'Общая физическая подготовка',
+    level: 'Любой',
+    master: 'Сергей',
+    note: 'с октября',
+  },
+  {
+    day: 'Сб',
+    time: '10:45',
+    duration: 90,
+    title: 'Кундалини-йога',
+    level: 'Любой',
+    master: 'Татьяна',
+  },
+  {
+    day: 'Сб',
+    time: '12:30',
+    duration: 90,
+    title: 'Хатха-йога для начинающих',
+    level: 'Лёгкий',
+    master: 'Евгения',
+  },
+  {
+    day: 'Сб',
+    time: '14:15',
+    duration: 90,
+    title: 'Здоровая спина (йога-терапия)',
+    level: 'Лёгкий',
+    master: 'Евгения',
+  },
+  {
+    day: 'Вс',
+    time: '19:00',
+    duration: 90,
+    title: 'Хатха-йога для начинающих',
+    level: 'Лёгкий',
+    master: 'Надежда',
+    note: 'по предварительной записи',
+  },
+].map(({ day, time: from, duration, ...i }) => {
+  const [ h, m ] = from.split(':').map(Number)
+  const time = h * 60 + m
+  const weekday = [ 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс' ].indexOf(day)
+
+  const id = `${day}-${time}-${i.master}-${i.title}`
+  const to = format(time + duration)
+  return { ...i, id, time, from, to, weekday }
+}) as Lesson[]
+
+
+// Преобразуем список занятий в объект
+export const getLessons: any = (list: Lesson[]) => {
+  const lessons: Lesson[][] =  list
+    .reduce((r: Lesson[][], i: Lesson) => {
+      if (!Array.isArray(r[i.weekday])) r[i.weekday] = []
+      r[i.weekday].push(i)
+      return r
+    }, [])
+
+  // сортируем по началу и продолжительности
+  // то есть если два занятия начинаются в одно и то же время,
+  // то первым будет то, которое раньше заканчивается
+  const compare = (a: Lesson, b: Lesson) => {
+    if (a.time < b.time) return -1
+    if (a.time > b.time) return 1
+    if (a.duration < b.duration) return -1
+    if (a.duration > b.duration) return 1
+    return 0
+  }
+
+  return lessons.map(i => i.sort(compare))
+}
+
+
+export default getLessons(lessons)
--- app/schedule/page.tsx
+++ app/schedule/page.tsx
@@ -1,10 +1,143 @@
+'use client'
+
+import React from 'react'
+
 import { Container } from '@/components/Container'
-import { Title } from '@/components/Title'
+import lessons, { Lesson, getDay, weekdays } from './lessons'
+import { Button } from '@/components/ui/button'
+import { cn } from '@/lib/utils'
 
 const SchedulePage = () => {
-  return <Container className="mt-10">
-    <Title size="md">Schedule</Title>
-  </Container>
+  const [ day, setDay ] = React.useState(getDay())
+  const [ view, setView ] = React.useState('day') // day or week
+
+  const table = lessons.reduce((r: any, dayLessons: Lesson[], weekday: any) => {
+    dayLessons.forEach((lesson: Lesson) => {
+      const hour: number = lesson.time / 60 | 0
+      if (!r[hour]) r[hour] = { [weekday]: [ lesson ] }
+      else if (!Array.isArray(r[hour][weekday])) r[hour][weekday] = [ lesson ]
+      else r[hour][weekday] = [ ...r[hour][weekday], lesson ]
+    })
+    return r
+  }, [])
+
+  if (view === 'day') return (
+    <Container
+      className="text-center m-3"
+      /*title="Расписание | Йога клуб ОМ"*/
+    >
+      <Button
+        className="mb-3 bg-brand-400 hover:bg-brand-400/90"
+        onClick={() => setView('week')}
+      >
+        Показать всю неделю
+      </Button>
+      <ul className="grid grid-cols-7 rounded-md mb-2 shadow-[#e7eaf3_0px_1px_1px_inset,#e7eaf3_0px_0px_8px]">
+        {
+          weekdays.map((weekday, i) => (
+            <li
+              key={weekday}
+              className={cn(i === day && 'bg-[#fbe1c2]', 'cursor-pointer inline-block font-semibold p-2')}
+              onClick={() => setDay(i)}
+            >
+              {weekday}
+            </li>
+          ))
+        } 
+      </ul>
+      <ul>
+        {
+          lessons[day].map((i: any) => (
+            <li
+              key={i.id}
+              className="p-5 my-3 rounded-md shadow-[#e7eaf3_0px_1px_1px_inset,#e7eaf3_0px_0px_8px]"
+            >
+              <div className="text-[#468ee5] font-bold">{i.from} - {i.to}</div>
+              <div>{i.title}</div>
+              <div className="font-light">{i.master}</div>
+              <div>{i.alternate}</div>
+              <div>{i.level}</div>
+              <div>{i.note}</div>
+            </li>
+          ))
+        }
+      </ul>
+    </Container>
+  )
+  if (view === 'week') return (
+    <Container
+      className="text-center m-3"
+    >
+      <Button
+        className="mb-3 bg-brand-400 hover:bg-brand-400/90"
+        onClick={() => setView('day')}
+      >
+        Показать один день
+      </Button>
+
+      <table
+        className="text-xs"
+      >
+        <thead>
+          <tr>
+            <th>T</th>
+            {
+              weekdays.map((weekday, i) => (
+                <th
+                  key={weekday}
+                  className={cn(i === day && 'bg-[#fbe1c2]', 'cursor-pointer font-semibold p-2')}
+                  onClick={() => setDay(i)}
+                >
+                  {weekday}
+                </th>
+              ))
+            }
+          </tr>
+        </thead>
+        <tbody>
+          {
+            table.map((row: any, index: number) => (
+              <tr
+                key={index}
+                className={cn(index % 2 && 'bg-[#8a78730d]')}
+              >
+                <td className="p-2">{index}</td>
+                {
+                  [ 0, 1, 2, 3, 4, 5, 6 ].map(weekday => (
+                    <td
+                      key={weekday}
+                      className={cn(weekday === day && 'bg-[#fbe1c222]', 'cursor-pointer font-semibold p-2')}
+                    >
+                      <ul>
+                        {
+                          row[weekday]?.filter((i: Lesson) => !i.hidden)?.map((i: Lesson) => (
+                            <li
+                              key={i.id}
+                              className="p-5 my-3 rounded-md shadow-[#e7eaf3_0px_1px_1px_inset,#e7eaf3_0px_0px_8px]"
+                            >
+                              <div className="text-[#468ee5] font-bold">{i.from} - {i.to}</div>
+                              <div>{i.title}</div>
+                              <div className="font-light">{i.master}</div>
+                              <div>{i.alternate}</div>
+                              <div>{i.level}</div>
+                              <div>{i.note}</div>
+                            </li>
+                          ))
+                        }
+                      </ul>
+                    </td>
+                  ))
+                }
+              </tr>
+            ))
+          }
+        </tbody>
+      </table>
+
+    </Container>
+  )
+  return <pre>{JSON.stringify(table, null, 4)}</pre>
 }
+
 
 export default SchedulePage
