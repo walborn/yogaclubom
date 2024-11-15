77ecff06cc658349e2386e4475b710306e908bfd
Boris <yuzhakov.boris@gmail.com>
2024-11-15 18:48:43 +0300

    fix: change styles for schedule

--- app/schedule/page.tsx
+++ app/schedule/page.tsx
@@ -22,22 +22,19 @@ const SchedulePage = () => {
   }, [])
 
   if (view === 'day') return (
-    <Container
-      className="text-center m-3"
-      /*title="Расписание | Йога клуб ОМ"*/
-    >
+    <Container className={'text-center p-5'}>
       <Button
-        className="mb-3 bg-brand-400 hover:bg-brand-400/90"
+        className="mb-5 bg-brand-400 hover:bg-brand-400/90"
         onClick={() => setView('week')}
       >
         Показать всю неделю
       </Button>
-      <ul className="grid grid-cols-7 rounded-md mb-2 shadow-[#e7eaf3_0px_1px_1px_inset,#e7eaf3_0px_0px_8px]">
+      <ul className="grid grid-cols-7 rounded-md mb-2 shadow-light">
         {
           weekdays.map((weekday, i) => (
             <li
               key={weekday}
-              className={cn(i === day && 'bg-[#fbe1c2]', 'cursor-pointer inline-block font-semibold p-2')}
+              className={cn(i === day && 'bg-[#fbe1c2]', 'text-base cursor-pointer inline-block font-semibold p-2')}
               onClick={() => setDay(i)}
             >
               {weekday}
@@ -50,7 +47,7 @@ const SchedulePage = () => {
           lessons[day].map((i: any) => (
             <li
               key={i.id}
-              className="p-5 my-3 rounded-md shadow-[#e7eaf3_0px_1px_1px_inset,#e7eaf3_0px_0px_8px]"
+              className="p-5 my-3 rounded-md shadow-light"
             >
               <div className="text-[#468ee5] font-bold">{i.from} - {i.to}</div>
               <div>{i.title}</div>
@@ -65,76 +62,80 @@ const SchedulePage = () => {
     </Container>
   )
   if (view === 'week') return (
-    <Container
-      className="text-center m-3"
-    >
-      <Button
-        className="mb-3 bg-brand-400 hover:bg-brand-400/90"
-        onClick={() => setView('day')}
-      >
+    <>
+      <Container className="text-center">
+        <Button
+          className="mt-5 bg-brand-400 hover:bg-brand-400/90"
+          onClick={() => setView('day')}
+        >
         Показать один день
-      </Button>
-
-      <table
-        className="text-xs"
+        </Button>
+      </Container>
+      
+      <Container
+        className="text-center p-5 max-w-full overflow-x-scroll"
       >
-        <thead>
-          <tr>
-            <th>T</th>
+        <table
+          className="text-xs rounded shadow-light"
+        >
+          <thead>
+            <tr>
+              <th>T</th>
+              {
+                weekdays.map((weekday, i) => (
+                  <th
+                    key={weekday}
+                    className={cn(i === day && 'bg-[#fbe1c2]', 'text-base cursor-pointer font-semibold p-2')}
+                    onClick={() => setDay(i)}
+                  >
+                    {weekday}
+                  </th>
+                ))
+              }
+            </tr>
+          </thead>
+          <tbody>
             {
-              weekdays.map((weekday, i) => (
-                <th
-                  key={weekday}
-                  className={cn(i === day && 'bg-[#fbe1c2]', 'cursor-pointer font-semibold p-2')}
-                  onClick={() => setDay(i)}
+              table.map((row: any, index: number) => (
+                <tr
+                  key={index}
+                  className={cn(index % 2 && 'bg-[#8a78730d]')}
                 >
-                  {weekday}
-                </th>
+                  <td className="p-2">{index}</td>
+                  {
+                    [ 0, 1, 2, 3, 4, 5, 6 ].map(weekday => (
+                      <td
+                        key={weekday}
+                        className={cn(weekday === day && 'bg-[#fbe1c222]', 'cursor-pointer font-semibold p-2')}
+                      >
+                        <ul>
+                          {
+                            row[weekday]?.filter((i: Lesson) => !i.hidden)?.map((i: Lesson) => (
+                              <li
+                                key={i.id}
+                                className="p-5 my-3 rounded-md shadow-[#e7eaf3_0px_1px_1px_inset,#e7eaf3_0px_0px_8px]"
+                              >
+                                <div className="text-[#468ee5] font-bold">{i.from} - {i.to}</div>
+                                <div>{i.title}</div>
+                                <div className="font-light">{i.master}</div>
+                                <div>{i.alternate}</div>
+                                <div>{i.level}</div>
+                                <div>{i.note}</div>
+                              </li>
+                            ))
+                          }
+                        </ul>
+                      </td>
+                    ))
+                  }
+                </tr>
               ))
             }
-          </tr>
-        </thead>
-        <tbody>
-          {
-            table.map((row: any, index: number) => (
-              <tr
-                key={index}
-                className={cn(index % 2 && 'bg-[#8a78730d]')}
-              >
-                <td className="p-2">{index}</td>
-                {
-                  [ 0, 1, 2, 3, 4, 5, 6 ].map(weekday => (
-                    <td
-                      key={weekday}
-                      className={cn(weekday === day && 'bg-[#fbe1c222]', 'cursor-pointer font-semibold p-2')}
-                    >
-                      <ul>
-                        {
-                          row[weekday]?.filter((i: Lesson) => !i.hidden)?.map((i: Lesson) => (
-                            <li
-                              key={i.id}
-                              className="p-5 my-3 rounded-md shadow-[#e7eaf3_0px_1px_1px_inset,#e7eaf3_0px_0px_8px]"
-                            >
-                              <div className="text-[#468ee5] font-bold">{i.from} - {i.to}</div>
-                              <div>{i.title}</div>
-                              <div className="font-light">{i.master}</div>
-                              <div>{i.alternate}</div>
-                              <div>{i.level}</div>
-                              <div>{i.note}</div>
-                            </li>
-                          ))
-                        }
-                      </ul>
-                    </td>
-                  ))
-                }
-              </tr>
-            ))
-          }
-        </tbody>
-      </table>
+          </tbody>
+        </table>
+      </Container>
+    </>
 
-    </Container>
   )
   return <pre>{JSON.stringify(table, null, 4)}</pre>
 }
