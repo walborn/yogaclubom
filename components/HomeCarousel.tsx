d9e964184d681bfb02c79de36658c336ed0fcfc2
Boris Yuzhakov <yuzhakov.boris@gmail.com>
2025-05-27 19:43:25 +0300

    feat: add birthday home image

--- components/HomeCarousel.tsx
+++ components/HomeCarousel.tsx
@@ -34,6 +34,7 @@ export const HomeCarousel: React.FC<Props> = ({ className }) => {
           // '/images/home/cancellation.jpeg',
           // '/images/home/new_year.jpeg',
           // '/images/home/may.jpeg',
+          '/images/home/birthday.jpeg',
           '/images/home/virabhadrasana.jpeg',
           '/images/home/healthy_back.jpeg',
           '/images/home/jeet_kune_do.jpeg',
