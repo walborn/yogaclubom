e1959c90b04f49de18143d1f9993b338b0df7561
Boris Yuzhakov <yuzhakov.boris@gmail.com>
2025-06-03 19:06:05 +0300

    fix: security

--- lib/placeholder.users3.ts
+++
@@ -1,9 +0,0 @@
-import type { User } from '@/lib/definitions'
-
-export const users: User[] = [
-  {
-    name: 'Irina',
-    email: 'irina@yogaclubom.ru',
-    password: '3dGTNQiVjaK8r2QKw4eUxlQ',
-  },
-]
