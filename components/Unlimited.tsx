2bf12bf8c11e8ae839136c510522645018ff797f
irinasklizkova <irinasklizkova@gmail.com>
2025-12-05 10:50:23 +0300

    05.12.25-1

--- components/Unlimited.tsx
+++ components/Unlimited.tsx
@@ -14,7 +14,7 @@ export const Unlimited: React.FC<Props> = ({ className }) => {
   return (
     <section className={cn('md:flex md:gap-2', className)}>
       <List className="flex-1 max-md:mb-5 flex flex-col gap-2 custom-bullet">
-        <li>Стоимость <strong>1000</strong> рублей</li>
+        <li>Стоимость <strong>1200</strong> рублей</li>
         <li>Акция актуальна, если вы у нас впервые</li>
         <li>2 занятия в течение 7 дней. Нельзя переносить, замораживать и продлевать</li>
         <li>Мы будем благодарны за любой честный отзыв в удобном для вас сервисе (2ГИС, яндекс карты)</li>
