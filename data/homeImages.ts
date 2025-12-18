1beb6bfec170f6fa6dc52fa2d73edbac170e346b
irinasklizkova <irinasklizkova@gmail.com>
2025-12-18 17:46:50 +0300

    18.12.25-2

--- data/homeImages.ts
+++ data/homeImages.ts
@@ -1,6 +1,6 @@
 export const images: string[] = [
   'images/home/best-gift-for-yoga-girl.webp',
-  'images/home/virabhadrasana.webp',
+  'images/home/31.12-7.01.JPG',
   'images/home/healthy_back.webp',
   'images/home/jeet_kune_do.webp',
   'images/home/sarvangasana.webp',
