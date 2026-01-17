6d7d9ba582c8bdb43fda5648bcf1f63184e62dd1
irinasklizkova <irinasklizkova@gmail.com>
2026-01-17 12:36:35 +0300

    17.01.26-1

--- data/lessons.ts
+++ data/lessons.ts
@@ -48,7 +48,7 @@ const lessons: Lesson[] = [
     title: 'Хатха-йога',
     level: 'Любой',
     master: 'Алексей',
-    notes: 'по предварительной записи',
+    notes: '20 января отмена',
   },
   {
     weekday: 'Вт',
@@ -97,7 +97,7 @@ const lessons: Lesson[] = [
     title: 'Хатха-йога',
     level: 'Любой',
     master: 'Алексей',
-    notes: '15 января отмена',
+    notes: '22 января отмена',
   },
   {
     weekday: 'Чт',
