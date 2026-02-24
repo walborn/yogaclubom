b5b42ef0ef5fb5ca89be3997deb3edb4a57c0c5c
irinasklizkova <irinasklizkova@gmail.com>
2026-02-24 13:27:20 +0300

    24.02.26-1

--- data/lessons.ts
+++ data/lessons.ts
@@ -47,7 +47,7 @@ const lessons: Lesson[] = [
     finish: '19:25',
     title: 'Хатха-йога с акцентом на медитацию',
     level: 'Любой',
-    master: 'Андрей',
+    master: 'Алексей',
     notes: 'по предварительной записи',
   },
   {
@@ -183,7 +183,7 @@ const lessons: Lesson[] = [
     finish: '12:30',
     title: 'Хатха-йога',
     level: 'Любой',
-    master: 'Алексей',
+    master: 'Ксения',
     notes: 'по предварительной записи',
   },
   {
